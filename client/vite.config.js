edbeacf304270d5070c53241d51d71e8ac9e20cb
tejas-kumar-gh <dhimantejas1234@gmail.com>
2026-05-09 22:18:25 +0530

    changes in package.json in frontend

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  base: '/',
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
